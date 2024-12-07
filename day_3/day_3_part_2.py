eb0ac379070e4387f774cbf0c90f74d86a2864e3
Annika Zdon <azdon@umich.edu>
2024-12-07 15:31:43 -0500

    append all input into 1 line

--- day_3/day_3_part_2.py
+++ day_3/day_3_part_2.py
@@ -1,30 +1,41 @@
 import re
 
 # input
-#sample_input_part2_test
-with open('input/sample_input_part2_test.txt', 'r') as file:
+# sample_input_part2_test
+# prev too  high answer
+
+with open('input/sample_input_part2.txt', 'r') as file:
     pattern = r"mul\([0-9]{1,3}[,][0-9]{1,3}\)"
     do_pattern = r"do\(\)"
     dont_do_pattern = r"don't\(\)"
     
     safe_chunks = []
     total_sum = 0
+    mega_line = ""
+    
     for line in file:
-        # Find safe mul operations by finding chunks where dont() is
-        line_chunks = line.split("don't()")
-        print(f"line after split on don't(): {line_chunks}")
-        safe_chunks.append(line_chunks[0])
+        mega_line += line
         
-        # Next safe chunk could be within the rest of the split array
-        for chunk in line_chunks[1:]:
-            line = chunk.split("do()")
-            print(f"line split from within a dont() substring after splitting on do(): {line} from chunk: {chunk}")
-            # print(line)
-            if len(line) > 1:
-                new_safe_chunk = line[1]
-                print(f"safe chunk to mul: {new_safe_chunk}")
-                safe_chunks.append(new_safe_chunk)
+        
+    # Find safe mul operations by finding chunks where dont() is
+    line_chunks = mega_line.split("don't()")
+    print(f"line after split on don't(): {line_chunks}")
+    safe_chunks.append(line_chunks[0])
+    
+    # Next safe chunk could be within the rest of the split array
+    for chunk in line_chunks[1:]:
+        line = chunk.split("do()")
+        print(f"line split from within a dont() substring after splitting on do(): {line} from chunk: {chunk}")
+        # print(line)
+        if len(line) > 1:
+            for safe_chunk in line[1:]:
+                print(f"safe chunk to mul: {safe_chunk}")
+                safe_chunks.append(safe_chunk)
                 
+            # new_safe_chunk = line[1]
+            # print(f"safe chunk to mul: {new_safe_chunk}")
+            # safe_chunks.append(new_safe_chunk)
+            
         
         print(f"safe chunks: {safe_chunks}")
         for chunk in safe_chunks:
@@ -42,3 +53,5 @@ with open('input/sample_input_part2_test.txt', 'r') as file:
                 # print(f"Parsed num1 {num1} * parsed num2 {num2} = sub product {product}")
         
     print(f"Total sum: {total_sum}")
+    
+
